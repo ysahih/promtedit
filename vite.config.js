115ff7eb374d31d672f73fea82f5915be248c5ba
ysahih <youssefsahih9@gmail.com>
2026-03-05 22:39:00 +0000

    feat: Add GitHub Pages deployment workflow and configure Vite base path for project.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: '/promtedit/',
     plugins: [
         tailwindcss(),
     ],
