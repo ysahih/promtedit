738a7fb2fbfa730f2bb52275ddca2a88adc0b2c4
ysahih <youssefsahih9@gmail.com>
2026-03-05 23:05:30 +0000

    build: set Vite base path to relative

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/promtedit/',
+    base: './',
     plugins: [
         tailwindcss(),
     ],
